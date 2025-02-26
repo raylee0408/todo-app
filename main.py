9ba52a21d76a82d6462114b0b13c18e6ee3a8a58
RayLee <raylee598+git@gmail.com>
2025-02-27 06:01:58 +1300

    Add time label feature

---
+++ main.py
@@ -0,0 +1,76 @@
+# from functions import get_todos,write_todos
+import functions
+import time
+
+now = time.strftime("%d/%b/%Y, %H:%M:%S")
+print("It is", now)
+print("IT is, now")
+while True:
+    user_action = input("Type add edit exit or show: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo=user_action[4:]
+
+        todos = functions.get_todos()
+
+        todos.append(todo+'\n')
+
+        functions.write_todos(todos)
+
+    elif user_action.startswith("show"):
+
+        todos = functions.get_todos()
+
+        for index , item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1 }.{item}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+
+            number = number - 1
+
+            todos = functions.get_todos()
+
+            new_todo = input("Enter new todo: ") + "\n"
+            todos[number] = new_todo
+
+            functions.write_todos(todos)
+
+        except ValueError:
+            print("Your Command is not valid. ")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = functions.get_todos()
+            index = number -1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+
+            functions.write_todos(todos)
+
+
+            message = f"Todo #{number} {todo_to_remove} is complete"
+            print(message)
+        except IndexError:
+            print("There is no item with that number")
+            continue
+
+
+
+    elif user_action.startswith("exit"):
+        print("bye")
+        break
+
+    else:
+        print("Command is not valid")
+print("")
+
+
