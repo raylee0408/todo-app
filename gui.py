8d9a3b9528bac21f37f6f9bed33f64d9e3fa1c9b
RayLee <raylee598+git@gmail.com>
2025-03-04 06:12:28 +1300

    change theme

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import functions
 import FreeSimpleGUI as sq
+import time
 
+sq.theme("Black")
+
+
+clock = sq.Text("",key='clock')
 label = sq.Text("Type in a to-do")
 input_box = sq.InputText(tooltip="Enter Todo",key="todo")
 add_button = sq.Button("Add")
@@ -12,17 +17,17 @@ exit_button = sq.Button("exit")
 
 
 window = sq.Window('My To-Do App',
-                   layout=[[label],
+                   layout=[[clock],
+                            [label],
                            [input_box,add_button],
                            [list_box,edit_button,complete_button],
                            [exit_button]],
                    font=("Helvetica",20))
 
 while True:
-    event,values= window.read()
-    print(1,event)
-    print(2, values)
-    print(3, values["todo"])
+    event,values= window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -32,26 +37,33 @@ while True:
             window["todos"].update(values=todos)
             window["todo"].update(value="")
         case "Edit":
-            todo_to_edit = values["todos"][0]
-            new_todo = values["todo"]
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values["todo"]
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+            except IndexError:
+                sq.popup("Please Select the item first", font=("Helvetica",20))
 
         case "Complete":
-            todo_to_complete = values["todos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value="")  #only 1 value, so use value, not values
+            try:
+                todo_to_complete = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")  #only 1 value, so use value, not values
+            except IndexError:
+                sq.popup("Please Select the item first", font=("Helvetica", 20))
+        case "Exit":
+            break
 
         case "todos":  # click event "todos" when click the list and show on the top
             window["todo"].update(value=values["todos"][0])
-        case "Exit":
-            break
+
         case sq.WIN_CLOSED:
             break
 
